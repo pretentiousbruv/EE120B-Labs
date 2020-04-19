2a038011748113d7d08d35cb5ce22e4c89732840
Yaobin Li <52799475+pretentiousbruv@users.noreply.github.com>
2020-04-19 01:29:33 -0700

    Update main.c

--- Lab4_StateMach/source/main.c
+++ Lab4_StateMach/source/main.c
@@ -12,31 +12,76 @@
 #include "simAVRHeader.h"
 #endif
 
+enum LED_States{Start, offrelease, onpress, onrelease, offpress} LED_State;
+
+void LED_Status(){
+	switch(LED_State){ //transitions
+		case Start:
+			LED_State = offrelease;
+			break;
+		case offrelease:
+			if(PINA == 0x01){
+				LED_State = onpress;
+			}
+			if(PINA == 0x00){
+				LED_State = offrelease;
+			}
+			break;
+		case onpress:
+			if(PINA == 0x01){
+				LED_State = onpress;
+			}
+			if(PINA == 0x00){
+				LED_State = onrelease;
+			}
+			break;
+		case onrelease:
+			if(PINA == 0x01){
+				LED_State = offpress;
+			}
+			if(PINA == 0x00){
+				LED_State = onrelease;
+			}
+			break;
+		case offpress:
+			if(PINA == 0x01){
+				LED_State = offpress;
+			}
+			if(PINA == 0x00){
+				LED_State = offrelease;
+			}
+			break;
+		default:
+			LED_State = Start;
+			break;
+	}
+	switch(LED_State){ //state actions
+		case offrelease:
+			PORTB = 0x01;
+			break;
+		case onpress:
+			PORTB = 0x02;
+			break;
+		case onrelease:
+			PORTB = 0x02;
+			break;
+		case offpress:
+			PORTB = 0x01;
+			break;
+		default:
+			break;
+	}
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00;	PORTA = 0xFF;
 	DDRB = 0xFF;	PORTB = 0x00;
     /* Insert your solution below */
-	unsigned char tempA = 0x00;
-	unsigned char button = 0x00;
-	unsigned char count = 0x00;
+	PORTB = 0x01;
+	LED_State = Start;
     while (1) {
-	tempA = PINA;
-	
-	if(tempA == 0x01){
-		button = 0x01;
-		count = count + 1;
-		if(count > 3){
-			button = 0x00;
-			count = 0x00;
-		}
-	}
-	if(button == 0x01){
-		PORTB = 0x02;	
-	}
-	if(button == 0x00){
-		PORTB = 0x01;
-	}
+	LED_Status();
     }
     return 1;
 }
