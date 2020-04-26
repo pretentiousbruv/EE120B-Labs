0d387c530aaf3188d0ac9e7f83401a04fbb2572e
Yaobin Li <52799475+pretentiousbruv@users.noreply.github.com>
2020-04-25 18:32:12 -0700

    Update yli504_lab5_part1.c

--- Lab5_ATMega/turnin/yli504_lab5_part1.c
+++ Lab5_ATMega/turnin/yli504_lab5_part1.c
@@ -13,11 +13,39 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+	DDRA = 0x00;	PORTA = 0xFF;
 
+	DDRC = 0xFF;	PORTC = 0x00;/* Insert DDR and PORT initializations */
+
+	unsigned char tempA = 0x00;
+	unsigned char tempC = 0x00;
     /* Insert your solution below */
     while (1) {
+	tempA = ~PINA;
+	tempA = tempA & 0x0F;
 
+	if((tempA < 16) && (tempA > 12)){
+		tempC = 0x3F;
+	}
+	else if((tempA < 13) && (tempA > 9)){
+		tempC = 0x3E;
+	}
+	else if((tempA < 10) && (tempA > 6)){
+		tempC = 0x3C;
+	}
+	else if((tempA < 7) && (tempA > 4)){
+		tempC = 0x38;
+	}
+	else if((tempA < 5) && (tempA > 2)){
+		tempC = 0x70;
+	}
+	else if((tempA < 3) && (tempA > 0)){
+		tempC = 0x60;
+	}
+	else{
+		tempC = 0x40;
+	}
+	PORTC = tempC;
     }
     return 1;
 }
