42494f2ef299848c99a5dad261ef27627d1a7026
Yaobin Li <52799475+pretentiousbruv@users.noreply.github.com>
2020-05-03 00:00:30 -0700

    Update yli504_lab5_part1.c

--- Lab5_ATMega/turnin/yli504_lab5_part1.c
+++ Lab5_ATMega/turnin/yli504_lab5_part1.c
@@ -2,8 +2,9 @@
  *  Partner(s) Name: 
  *	Lab Section:
  *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
+ *	part 1 demo: https://drive.google.com/file/d/1cxTVGz1nVPBtMkfSiLKkGEXmHenHclZo/view?usp=sharing
+ *	part 2 demo: 
+ *	part 3 demo: 
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
