725de75253182f18ef4bd7560350c3f07600d3fc
Yaobin Li <52799475+pretentiousbruv@users.noreply.github.com>
2020-05-08 23:02:13 -0700

    Update yli504_lab6_part1.c

--- Lab6_SynchSMs/turnin/yli504_lab6_part1.c
+++ Lab6_SynchSMs/turnin/yli504_lab6_part1.c
@@ -8,16 +8,6 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#include <avr/interrupt.h>/*	Author: yli504
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
-#include <avr/io.h>
 #include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
@@ -86,78 +76,6 @@ int main(void) {
 	i = i + 1;
 	if(i == 3){
 		i = 0;
-	}
-
-	tempB = ~tempB;
-	PORTB = tempB;
-	while(!TimerFlag);
-	TimerFlag = 0;
-    }
-    return 1;
-}
-
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
-
-volatile unsigned char TimerFlag =  0;
-
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
-
-void TimerOn(){
-	TCCR1B = 0x0B;
-	
-	OCR1A = 125;
-	
-	TIMSK1 = 0x02;
-
-	TCNT1 = 0;
-
-	_avr_timer_cntcurr = _avr_timer_M;
-	
-	SREG |= 0x80;
-}
-
-void TimerOff(){
-	TCCR1B = 0x00;
-}
-
-void TimerISR(){
-	TimerFlag = 1;
-}
-
-ISR(TIMER1_COMPA_vect){
-	_avr_timer_cntcurr--;
-	if(_avr_timer_cntcurr == 0){
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
-}
-
-void TimerSet(unsigned long M){
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
-
-int main(void) {
-	DDRB = 0xFF;
-	PORTB = 0x00;
-	TimerSet(1000);
-	TimerOn();
-	unsigned char tempB = 0x00;
-	//unsigned char i = 0x00;   
-    while (1) {
-	if(tempB == 0x00 || tempB == 0x04){
-		tempB = 0x01;
-	}
-
-	else if(tempB == 0x01){
-		tempB = 0x02;
-	}
-
-	else if(tempB == 0x02){
-		tempB = 0x04;
 	}
 
 	tempB = ~tempB;
