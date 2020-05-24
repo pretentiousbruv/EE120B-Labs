dbe726153d6837a9873ee5c433e872f99ad3a5eb
Yaobin Li <52799475+pretentiousbruv@users.noreply.github.com>
2020-05-24 12:09:46 -0700

    Update yli504_lab10_part1.c

--- Lab10_ConSynch/turnin/yli504_lab10_part1.c
+++ Lab10_ConSynch/turnin/yli504_lab10_part1.c
@@ -8,164 +8,139 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#include "io.h"
+#include "timer.h"
 #endif
 
-volatile unsigned char TimerFlag =  0;
+enum states1{start1, init1, LED1, LED2, LED3} state1;
+enum states2{start2, init2, On, Off} state2;
+enum states3{start3, combineLED} state3;
 
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
-unsigned char i = 0;
-unsigned char j = 0;
+unsigned char LEDpos = 0x00;
+unsigned char OnOff = 0x00;
+unsigned char count = 0x01;
+unsigned char sig = 0x00;
 
-void TimerOn(){
-	TCCR1B = 0x0B;
-	
-	OCR1A = 125;
-	
-	TIMSK1 = 0x02;
-
-	TCNT1 = 0;
-
-	_avr_timer_cntcurr = _avr_timer_M;
-	
-	SREG |= 0x80;
-}
-
-void TimerOff(){
-	TCCR1B = 0x00;
-}
-
-void TimerISR(){
-	TimerFlag = 1;
-}
-
-ISR(TIMER1_COMPA_vect){
-	_avr_timer_cntcurr--;
-	if(_avr_timer_cntcurr == 0){
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
-}
-
-void TimerSet(unsigned long M){
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
-
-enum states{Start, init, inc, dec, wait, reset} state;
-
-void status(){
-	switch(state){ //transitions
-		case Start:
-			state = init;
+void threeLEDs(){
+	switch(state1){
+		case start1:
+			state1 = init1;
 			break;
-		case init:
-			j = 0;
-			if(~PINA & 0x02){
-				state = dec;
+		case init1:
+			if(count & 0x01){
+				state1 = LED1;
 			}
-			else if(~PINA & 0x01){
-				state = inc;
+			if(count & 0x02){
+				state1 = LED2;
 			}
-			else if((~PINA & 0x03) == 0x03){
-				state = reset;
+			if(count & 0x04){
+				state1 = LED3;
 			}
 			else{
-				state = init;
+				state1 = init1;
 			}
 			break;
-		case inc:
-			if((~PINA & 0x01) && (~PINA & 0x02)){
-				state = reset;
-			}
-			else if(~PINA & 0x01){
-				state = wait;
-			}
-			else{
-				state = init;
-			}
+		case LED1:
+			count = 0x02;
 			break;
-		case dec:
-			if((~PINA & 0x01) && (~PINA & 0x02)){
-				state = reset;
-			}
-			else if(~PINA & 0x02){
-				state = wait;
-			}
-			else{
-				state = init;
-			}
+		case LED2:
+			count = 0x04;
 			break;
-		case wait:
-			j = j + 1;
-			if(((~PINA & 0x03) == 0x02) || ((~PINA & 0x03) == 0x01)){
-				if(j >= 4){
-					state = init;
-				}
-				else{
-					state = wait;
-				}
-			}
-			else{
-				state = init;
-			}
-			break;
-		case reset:
-			if((~PINA & 0x01) || (~PINA & 0x02)){
-				state = wait;
-			}
-			else{
-				state = init;
-			}
+		case LED3:
+			count = 0x01;
 			break;
 		default:
-			break;
+			break;		
 	}
-	switch(state){ //state actions
-		case init:
-			LCD_Cursor(1);
-			LCD_WriteData(i + '0');
-			break;	
-		case inc:
-			if(i < 9){
-				i = i + 1;
-				LCD_Cursor(1);
-				LCD_WriteData(i + '0');
-			}
+
+	switch(state1){
+		case LED1:
+			LEDpos = 0x01;
 			break;
-		case dec:
-			if(i > 0){
-				i = i - 1;
-				LCD_Cursor(1);
-				LCD_WriteData(i + '0');
-			}
+		case LED2:
+			LEDpos = 0x02;
 			break;
-		case reset:
-			i = 0;
+		case LED3:
+			LEDpos = 0x04;
 			break;
 		default:
 			break;
 	}
 }
 
-int main(void) {
+void blinkingLED(){
+	switch(state2){
+		case start2:
+			state2 = init2;
+			break;
+		case init2:
+			if(sig & 0x01){
+				state2 = On;
+			}
+			if(sig & 0x00){
+				state2 = Off;
+			}
+			else{
+				state2 = init2;
+			}
+			break;
+		case On:
+			sig = 0x00;
+			break;
+		case Off:
+			sig = 0x01;
+			break;
+		default:
+			break;
+	}
+	
+	switch(state2){
+		case On:
+			OnOff = 0x08;
+			break;
+		case Off:
+			OnOff = 0x00;
+			break;
+		default:
+			break;
+	}
+}
+
+void combine(){
+	switch(state3){
+		case start3:
+			state3 = combineLED;
+			break;
+		case combineLED:
+			break;
+		default:
+			break;
+	}
+
+	switch(state3){
+		case combineLED:
+			PINB = LEDpos | OnOff;
+			break;
+		default:
+			break;
+	}
+}
+
+int main(void){
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00;	PORTA = 0xFF;
-	DDRC = 0xFF;	PORTC = 0x00;
-	DDRD = 0xFF;	PORTD = 0x00;
+	DDRB = 0xFF;	PORTB = 0x00;
     /* Insert your solution below */
-	TimerSet(100);
+	TimerSet(1000);
 	TimerOn();
-	LCD_init();
-	
-    while (1) {
-	status();
-	while(!TimerFlag);
-	TimerFlag = 0;
+    while (1){
+	threeLEDs();
+	blinkingLED();
+	combine();
+	while(!TimerFlag){
+		TimerFlag = 0;
+	}
     }
     return 1;
 }
