8e4c1c97b1b404bd2e916b790f59f887e8844adf
Yaobin Li <52799475+pretentiousbruv@users.noreply.github.com>
2020-05-24 21:39:15 -0700

    Update yli504_lab10_part2.c

--- Lab10_ConSynch/turnin/yli504_lab10_part2.c
+++ Lab10_ConSynch/turnin/yli504_lab10_part2.c
@@ -8,100 +8,130 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#include "timer.h"
 #endif
 
-enum states1{start1, init1, LED1, LED2, LED3} state1;
-enum states2{start2, init2, On, Off} state2;
-enum states3{start3, combineLED} state3;
+volatile unsigned char TimerFlag =  0;
 
-unsigned char LEDpos = 0x00;
-unsigned char OnOff = 0x00;
-unsigned char count = 0;
-unsigned char sig = 0;
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
 
-void threeLEDs(){
-	switch(state1){
-		case start1:
-			state1 = init1;
-			break;
-		case init1:
-			if(count == 1){
-				state1 = LED1;
-			}
-			else if(count == 2){
-				state1 = LED2;
-			}
-			else if(count == 3){
-				count = 0;
-				state1 = LED3;
-			}
-			break;
-		case LED1:
-			count++;
-			state1 = init1;
-			break;
-		case LED2:
-			count++;
-			state1 = init1;
-			break;
-		case LED3:
-			count++;
-			state1 = init1;
-			break;
-		default:
-			break;		
-	}
+void TimerOn(){
+	TCCR1B = 0x0B;
+	
+	OCR1A = 125;
+	
+	TIMSK1 = 0x02;
 
-	switch(state1){
-		case LED1:
-			LEDpos = 0x01;
-			break;
-		case LED2:
-			LEDpos = 0x02;
-			break;
-		case LED3:
-			LEDpos = 0x04;
-			break;
-		default:
-			break;
+	TCNT1 = 0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	
+	SREG |= 0x80;
+}
+
+void TimerOff(){
+	TCCR1B = 0x00;
+}
+
+void TimerISR(){
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect){
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr == 0){
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
 	}
 }
 
-void blinkingLED(){
-	switch(state2){
-		case start2:
-			state2 = init2;
+void TimerSet(unsigned long M){
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+enum states{Start, state1, state2, state3, button, pause, buttonagain} state;
+unsigned char i = 0;
+unsigned char three = 0x00;
+
+void status(){
+	switch(state){
+		case Start:
+			state = state1;
 			break;
-		case init2:
-			if(sig == 1){
-				state2 = Off;
+		case state1:
+			if(i == 1){
+				state = state2;
 			}
-			else if(sig == 2){
-				sig = 0;
-				state2 = On;
+			i = i + 1;
+			break;
+		case state2:
+			if(i == 2){
+				state = state3;
 			}
+			i = i + 1;
 			break;
-		case Off:
-			sig++;
-			state2 = init2;
-			break;
-		case On:
-			sig++;
-			state2 = init2;
+		case state3:
+			if(i == 3){
+				i = 0;
+				state = state1;
+			}
+			i = i + 1;
 			break;
 		default:
 			break;
 	}
 	
+	switch(state){
+		case state1:
+			three = 0x01;
+			break;
+		case state2:
+			three = 0x02;
+			break;
+		case state3:
+			three = 0x04;
+			break;
+		default:
+			break;
+	}
+			
+}
+
+enum states22{start2, On, Off} state22;
+unsigned char j = 0;
+unsigned char blink = 0x00;
+
+void blinkingLED(){
 	switch(state2){
-		case On:
-			OnOff = 0x08;
+		case start2:
+			state22 = Off;
 			break;
 		case Off:
-			OnOff = 0x00;
+			if(j == 1){
+				state22 = On;
+			}
+			j = j + 1;
+			break;
+		case On:
+			if(j == 2){
+				j = 0;
+				state22 = Off;
+			}
+			j = j + 1;
+		default:
+			break;
+	}
+	
+	switch(state2){
+		case Off:
+			blink = ~blink;
+			break;
+		case On:
+			blink = ~blink;
 			break;
 		default:
 			break;
@@ -109,51 +139,43 @@ void blinkingLED(){
 }
 
 void combine(){
-	switch(state3){
-		case start3:
-			state3 = combineLED;
-			break;
-		case combineLED:
-			break;
-		default:
-			break;
+	if(blink == 0xFF){
+		PORTB  = three | 0x08;
 	}
-
-	switch(state3){
-		case combineLED:
-			PINB = LEDpos | OnOff;
-			break;
-		default:
-			break;
+	else{
+		PORTB = three;
 	}
 }
-
-int main(void){
-    /* Insert DDR and PORT initializations */
-	DDRA = 0x00;	PORTA = 0xFF;
-	DDRB = 0xFF;	PORTB = 0x00;
-	unsigned long LED_elapsedTime = 300;
-	unsigned long blink_elapsedTime = 1000;
-    /* Insert your solution below */
+int main(void) {
+	DDRA = 0x00;
+	PORTA = 0xFF;
+	DDRB = 0xFF;
+	volatile unsigned long LED_elapsedTime = 300;
+	volatile unsigned long blink_elapsedTime = 1000;
+	volatile unsigned long combinetogether = 100;
+	PORTB = 0x00;
 	TimerSet(100);
-	TimerOn();
-	state1 = start1;
-	state2 = start2;
-    while (1){
-	if(LED_elapsedTime >= 300){
-		threeLEDs();
+	TimerOn();	
+    while (1) {
+	if(LED_elapsedTime >= 1000){
+		status();
 		LED_elapsedTime = 0;
 	}
 	if(blink_elapsedTime >= 1000){
 		blinkingLED();
 		blink_elapsedTime = 0;
 	}
-	combine();
-	while(!TimerFlag){
-		TimerFlag = 0;
-		LED_elapsedTime += 100;
-		blink_elapsedTime += 100;
+	if(combinetogether >= 100){
+		combine();
+		combinetogether = 0;
 	}
+
+	while(!TimerFlag){
+	}
+		LED_elapsedTime += 1000;
+		blink_elapsedTime += 100;
+		combinetogether += 100;
+		TimerFlag = 0;
     }
     return 1;
 }
