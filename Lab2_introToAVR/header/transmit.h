847b900a66ae6b2c7400fde6c84296f1cdadc20e
Yaobin Li <52799475+pretentiousbruv@users.noreply.github.com>
2020-05-27 13:12:26 -0700

    Update transmit.h

--- Lab2_introToAVR/header/transmit.h
+++ Lab2_introToAVR/header/transmit.h
@@ -10,8 +10,21 @@ void Transmit()
     switch(transmit_state)
     {
         case TInit:
+		transmit_state = ping;
             break;
-        default:
+	case ping:
+		transmit_state = detect;
+		break;
+	case detect:
+		transmit_state = max_amp;
+		break;
+	case max_amp:
+		transmit_state = zero_cross;
+		break;
+	case zero_cross;
+		transmit_state = Tinit;
+		break;
+	default:
             transmit_state = TInit;
             break;
     }
@@ -19,6 +32,18 @@ void Transmit()
     {
         case TInit:
             break;
+	case ping:
+		PORTB = PORTB | ping_data;
+		break;
+	case detect:
+		PORTB = PORTB | detected;
+		break;
+	case max_amp:
+		PORTB = PORTB | max;
+		break;
+	case zero_cross:
+		PORTB = PORTB | cross;
+		break;
         default:
             break;
     }
