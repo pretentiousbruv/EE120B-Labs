682bb0e969d4fbb929aa520c309961f830d028c1
Yaobin Li <52799475+pretentiousbruv@users.noreply.github.com>
2020-05-27 00:51:17 -0700

    Update main.c

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -1,4 +1,4 @@
-/*	Author: yli504
+/*	Author: lab
  *  Partner(s) Name: 
  *	Lab Section:
  *	Assignment: Lab #  Exercise #
@@ -11,40 +11,42 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+#include "timer.h"
 
+/* SM state declarations --- fill in as needed */
+typedef enum output_states { OInit, Ooutput } output_states;
+typedef enum read_states { RInit, Rread } read_states;
+
+/* shared variables --- fill in as needed */
+unsigned char PA;
+
+/* state variables --- do not alter */
+output_states output_state;
+read_states read_state;
+
+/* SM definitions --- complete each task as a SM */
+#include "output.h"
+#include "read.h"
+
+/* main function --- do not alter */
 int main(void) {
-    /* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF; //input
-	DDRB = 0x00; PORTB = 0xFF;
-	DDRC = 0x00; PORTC = 0xFF;
+    /* DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
 
-	DDRD = 0xFF; PORTD = 0x00; //output
-	
-	unsigned char tempA = 0x00;
-	unsigned char tempB = 0x00;
-	unsigned char tempC = 0x00;
-	unsigned char tempD = 0x00;
-	unsigned short weight = 0;
-	
-    /* Insert your solution below */
+    // Common period for all tasks set to 100ms
+    TimerSet(100);
+    TimerOn();
+
+    // init state vars
+    output_state = OInit;
+    read_state = RInit;
+
     while (1) {
-	tempA = PINA;
-	tempB = PINB;
-	tempC = PINC;
-	tempD = 0x00;
-	weight = 0;
-
-	weight = tempA + tempB + tempC;
-	tempD = weight & 0xFC;
-	
-	if(weight > 140){
-		tempD = tempD | 0x01;
-	}
-	
-	if((tempA - tempC) > 80 || (tempC - tempA)){
-		tempD = tempD | 0x02;
-	}
-	PORTD = tempD
+        Read();
+        Output();
+        while (!TimerFlag) { }
+        TimerFlag = 0;
     }
     return 1;
 }
