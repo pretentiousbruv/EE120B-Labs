2a4b53aecb39799e8bb20c7e0fdf47ac5c92b420
Yaobin Li <52799475+pretentiousbruv@users.noreply.github.com>
2020-05-27 13:12:07 -0700

    Update detect_max_amp.h

--- Lab2_introToAVR/header/detect_max_amp.h
+++ Lab2_introToAVR/header/detect_max_amp.h
@@ -1,6 +1,11 @@
 //define local variable(s) here.  Use static keyword to keep local, e.g:
 //   static int i;  // defines a local int named i
-
+unsigned char temp = 0x00;
+unsigned char compare = 0x00;
+temp = ~PINA;
+temp = temp & 0xF8;
+compare = PORTB;
+compare = PORTB & 0xF8;
 /* complete the state machine. */
 
 void Detect_Max_Amp()
@@ -8,8 +13,19 @@ void Detect_Max_Amp()
     switch(detect_max_amp_state)
     {
         case DMAInit:
+		if(temp > compare){
+			detect_max_amp_state = new_max_amp;
+		}
+		else{
+			detect_max_amp_state = same_amp;
+		}
             break;
-        
+	case new_max_amp:
+		detect_max_amp_state = DMAInit;
+		break;
+        case same_amp:
+		detect_max_amp_state = DMAInit;
+		break;
         default:
             break;
     }
@@ -17,6 +33,12 @@ void Detect_Max_Amp()
     {
         case DMAInit:
             break;
+	case new_max_amp:
+		max = max | temp;
+		break;
+	case same_amp:
+		max;
+		break;
         default:
             break;
     }
