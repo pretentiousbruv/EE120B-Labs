1c8df22e311c018b3141bc0252e8ad59d5f1d28a
Yaobin Li <52799475+pretentiousbruv@users.noreply.github.com>
2020-05-27 13:11:36 -0700

    Update detect_eq.h

--- Lab2_introToAVR/header/detect_eq.h
+++ Lab2_introToAVR/header/detect_eq.h
@@ -1,7 +1,8 @@
 //define local variable(s) here.  Use static keyword to keep local, e.g:
 //   static int i;  // defines a local int named i
-
-
+unsigned char motion = 0x00;
+unsigned char counter = 0;
+motion = ~PINA & 0xF8;
 
 /*complete the state machine*/
 
@@ -10,7 +11,29 @@ void Detect_EQ()
     switch(detect_eq_state)
     {
         case DEQInit:
-            //init variable(s) here.
+		if(motion == 0x00){
+			detect_eq_state = noquake;
+		}
+		else{
+			detect_eq_state = quake;
+		}
+		break;
+	case noquake:
+		detect_eq_state = DEQInit;
+		counter = 0;
+		break;
+	case quake:
+		if(motion > 0x00){
+			detect_eq_state = quake;
+			counter = 0;
+		}
+		else{
+			counter++;
+			if(counter >= 10){
+				detect_eq_state = noquake;
+			}
+			detect_eq_state = quake;
+		}//init variable(s) here.
             break;
         default:
             detect_eq_state = DEQInit;
@@ -20,6 +43,12 @@ void Detect_EQ()
     {
         case DEQInit:
             break;
+	case noquake:
+		detected = detected & 0xFD;
+		break;
+	case quake:
+		detected = detected | 0x02;
+		break;
         default:
             break;
     }
